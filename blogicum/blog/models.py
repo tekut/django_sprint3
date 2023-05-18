161108a2cb5e2feb218dd6d27e2e1dbd1d8ba4e2
Aleksandr Tekutev <tekutiev.sasha@gmail.com>
2023-05-18 13:24:13 +0100

    Added admin

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
+User = get_user_model()
+
 
 # Абстрактная модель
 class BaseModel(models.Model):
     is_published = models.BooleanField(default=True,
                                        verbose_name='Опубликовано',
-                                       help_text='Снимите галочку, чтобы скрыть публикацию.',
+                                       help_text='Снимите галочку, '
+                                       'чтобы скрыть публикацию.',
                                        )
     created_at = models.DateTimeField(auto_now_add=True,
                                       verbose_name='Добавлено',
@@ -16,40 +19,15 @@ class BaseModel(models.Model):
         abstract = True
 
 
-# Публикация
-class Post(BaseModel):
-    title = models.CharField(max_length=256, verbose_name='Заголовок')
-    text = models.TextField(verbose_name='Текст')
-    pub_date = models.DateField(verbose_name='Дата и время публикации',
-                                help_text='Если установить дату и время в будущем — можно делать отложенные публикации.',
-                                )
-    author = models.ForeignKey(User,
-                               on_delete=models.CASCADE,
-                               verbose_name='Автор публикации',
-                               )
-    location = models.ForeignKey(Location,
-                                 on_delete=models.SET_NULL,
-                                 blank=True,
-                                 verbose_name='Местоположение',
-                                 )
-    category = models.ForeignKey(Category,
-                                 on_delete=models.SET_NULL,
-                                 verbose_name='Категория',
-                                 )
-
-    class Meta:
-        verbose_name = 'публикация'
-        verbose_name_plural = 'Публикации'
-
-
 # Тематическая категория
 class Category(BaseModel):
     title = models.CharField(max_length=256, verbose_name='Заголовок')
     description = models.TextField(verbose_name='Описание')
-    slug = models.SlugField(max_length=64,
-                            unique=True,
+    slug = models.SlugField(unique=True,
                             verbose_name='Идентификатор',
-                            help_text='Идентификатор страницы для URL; разрешены символы латиницы, цифры, дефис и подчёркивание.',
+                            help_text='Идентификатор страницы для URL; '
+                            'разрешены символы латиницы, цифры, '
+                            'дефис и подчёркивание.',
                             )
 
     class Meta:
@@ -66,9 +44,31 @@ class Location(BaseModel):
         verbose_name_plural = 'Местоположения'
 
 
-# Пользователь
-User = get_user_model()
+# Публикация
+class Post(BaseModel):
+    title = models.CharField(max_length=256, verbose_name='Заголовок')
+    text = models.TextField(verbose_name='Текст')
+    pub_date = models.DateField(verbose_name='Дата и время публикации',
+                                help_text='Если установить дату и время '
+                                'в будущем — можно делать отложенные '
+                                'публикации.',
+                                )
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               verbose_name='Автор публикации',
+                               )
+    location = models.ForeignKey(Location,
+                                 on_delete=models.SET_NULL,
+                                 blank=True,
+                                 verbose_name='Местоположение',
+                                 null=True,
+                                 )
+    category = models.ForeignKey(Category,
+                                 on_delete=models.SET_NULL,
+                                 verbose_name='Категория',
+                                 null=True,
+                                 )
 
-
-class Post(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    class Meta:
+        verbose_name = 'публикация'
+        verbose_name_plural = 'Публикации'
