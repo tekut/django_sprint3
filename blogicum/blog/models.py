57bcea8ad769ad8adf83ab062a1776e0fc0aafb5
Aleksandr Tekutev <tekutiev.sasha@gmail.com>
2023-05-19 12:09:24 +0100

    Loaded posts

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -54,11 +54,10 @@ class Location(BaseModel):
 class Post(BaseModel):
     title = models.CharField(max_length=256, verbose_name='Заголовок')
     text = models.TextField(verbose_name='Текст')
-    pub_date = models.DateField(verbose_name='Дата и время публикации',
-                                help_text='Если установить дату и время '
-                                'в будущем — можно делать отложенные '
-                                'публикации.',
-                                )
+    pub_date = models.DateTimeField(verbose_name='Дата и время публикации',
+                                    help_text='Если установить дату и'
+                                    ' время в будущем — можно делать'
+                                    ' отложенные публикации.',)
     author = models.ForeignKey(User,
                                on_delete=models.CASCADE,
                                verbose_name='Автор публикации',
