09b249d73987bd3324bde33c649bac39883a84b4
Aleksandr Tekutev <tekutiev.sasha@gmail.com>
2023-05-20 13:57:59 +0100

    Customized admin zone

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -7,7 +7,7 @@ def index(request):
     post_list = Post.objects.select_related(
         'category',
         'location',
-        'author'
+        'author',
     ).filter(
         pub_date__lte=timezone.now(),
         is_published=True,
@@ -18,12 +18,14 @@ def index(request):
 
 
 def post_detail(request, id):
-    post_list = get_object_or_404(Post.objects.select_related(
-        'location', 'author', 'category').filter(
+    post_list = get_object_or_404(
+        Post.objects.select_related(
+            'location', 'author', 'category').
+        filter(
             pub_date__lte=timezone.now(),
             is_published=True,
             category__is_published=True
-    ),
+            ),
         pk=id
     )
     context = {'post': post_list}
